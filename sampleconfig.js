c4ef381747212d512d55948eebfe46cfeab2a338
RedPer <aleksander.wiecaszek@gmail.com>
2019-05-08 21:10:58 +0200

    Add sample config.

---
+++ sampleconfig.js
@@ -0,0 +1,15 @@
+//Quiz questions and anwsers
+const questions = ["Question 1", "Question 2"];
+const anwsers = [["Anwser 1 to Question 1", "Anwser 2 to Question 1", "Anwser 3 to Question 1 (correct)", "Anwser 4 to Question 1"], ["Anwser 1 to Question 2", "Anwser 2 to Question 2 (correct)", "Anwser 3 to Question 2", "Anwser 4 to Question 2"]];
+const correct = [2, 1];
+
+//Site config
+const pageTitle = "Quiz";
+const pageLang = "en";
+
+//Anticheat config
+const enableAnticheat = true;
+const showAnticheatAlerts = true;
+const showAnticheatWarnings = true;
+const defocusTimeLimit = 10;
+const defocusLimit = 10;
